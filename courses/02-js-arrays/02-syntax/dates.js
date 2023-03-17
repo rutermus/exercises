b53ad161868d2c333e25091f2a6e4ddd176d75c1
termus <rutermus@gmail.com>
2023-03-17 13:26:13 +0300

    maked default export

--- courses/02-js-arrays/02-syntax/dates.js
+++ courses/02-js-arrays/02-syntax/dates.js
@@ -1,6 +1,8 @@
-export const getWeekends = (entryType = 'long') => {
+const getWeekends = (entryType = 'long') => {
   const longType = ['saturday', 'sunday'];
   const shortType = ['sat', 'sun'];
   const result = entryType === 'short' ? shortType : longType;
   return result;
 };
+
+export default getWeekends;
