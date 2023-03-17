448469adfbac1540f43e25f3967ba9e7c4747f39
termus <rutermus@gmail.com>
2023-03-17 20:59:42 +0300

    added methods-chain exercise

---
+++ courses/01-js-basics/21-methods-chain/solution.js
@@ -0,0 +1,10 @@
+/* eslint no-console: 0 */
+
+const text = 'When \t\n you play a \t\n game of thrones you win or you die.';
+
+const result = text
+  .slice(4, 16)
+  .trim()
+  .length;
+
+console.log(result);
