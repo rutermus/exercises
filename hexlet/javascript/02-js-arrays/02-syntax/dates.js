72b91ad21e7354dd3b988203a524b812e649b28d
termus <rutermus@gmail.com>
2023-03-17 11:55:15 +0300

    add 02-syntax

---
+++ hexlet/javascript/02-js-arrays/02-syntax/dates.js
@@ -0,0 +1,6 @@
+export const getWeekends = (entryType = 'long') => {
+  const longType = ['saturday', 'sunday'];
+  const shortType = ['sat', 'sun'];
+  const result = entryType === 'short' ? shortType : longType;
+  return result;
+};
