861009d469682cadcc0141d4f2721f5fd0243b8f
termus <rutermus@gmail.com>
2023-03-17 15:33:41 +0300

    added naming exercise

---
+++ courses/01-js-basics/09-naming/solution.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-console */
+/* eslint-disable prefer-template */
+
+const king = 'King Balon the 6th';
+
+const castlesCount = 6;
+const roomPerCastle = 17;
+
+console.log(
+  `${king} has ${castlesCount * roomPerCastle} rooms.`,
+);
