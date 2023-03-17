fd393e7d06a16bd4f2ceaf0cc3c73e0517f032be
termus <rutermus@gmail.com>
2023-03-17 16:55:26 +0300

    added call-funtion-expression exercise

---
+++ courses/01-js-basics/16-call-function-expression/solution.js
@@ -0,0 +1,6 @@
+/* eslint-disable no-console */
+
+const text = 'Never forget what you are, for surely the world will not';
+
+const result = `${text[0]}${text[text.length - 1]}`;
+console.log(result);
