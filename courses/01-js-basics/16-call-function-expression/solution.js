040c4a15c675cda8c847803124a07ea8b76fa236
termus <rutermus@gmail.com>
2023-03-17 16:57:58 +0300

    fixed bug

--- courses/01-js-basics/16-call-function-expression/solution.js
+++ courses/01-js-basics/16-call-function-expression/solution.js
@@ -2,5 +2,5 @@
 
 const text = 'Never forget what you are, for surely the world will not';
 
-const result = `${text[0]}${text[text.length - 1]}`;
+const result = `First: ${text[0]}\nLast: ${text[text.length - 1]}`;
 console.log(result);
