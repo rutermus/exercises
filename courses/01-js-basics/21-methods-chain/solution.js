b54daa04c0f5beee727ce994d99989388f20729e
termus <rutermus@gmail.com>
2023-04-01 22:32:21 +0300

    fixed syntax

--- courses/01-js-basics/21-methods-chain/solution.js
+++ courses/01-js-basics/21-methods-chain/solution.js
@@ -1,10 +1,6 @@
 /* eslint no-console: 0 */
 
 const text = 'When \t\n you play a \t\n game of thrones you win or you die.';
-
-const result = text
-  .slice(4, 16)
-  .trim()
-  .length;
+const result = text.slice(4, 16).trim().length;
 
 console.log(result);
